809216e9cd7d42b835fe30e45aaa61ee60e1dd5e
zhiqiangyao <yaozqiang2021@163.com>
2023-07-10 09:13:01 +0800

    待开发

---
+++ src/main/java/org/yao/font_mac/FontMacApplication.java
@@ -0,0 +1,16 @@
+package org.yao.font_mac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @ClassName: FontMacApplication.java
+ * @Author: Ninllo
+ * @Date: 2023/6/28 10:06
+ */
+@SpringBootApplication
+public class FontMacApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FontMacApplication.class);
+    }
+}
